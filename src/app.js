bdcc19b04f94b7d2cd998b9d9bdd5ef3e7bf49a6
CrisIntriago <caintria@espol.edu.ec>
2024-03-04 19:27:13 -0500

    modificacion en cors

--- src/app.js
+++ src/app.js
@@ -14,17 +14,21 @@ const whitelist = [process.env.FRONTEND_URL , "190.154.6.111", "159.203.190.84"]
 
 const corsOptions = {
 
-    
     origin: function (origin, callback) {
-        if (whitelist.includes(origin)) {
-            // Puede consultar la API
-            callback(null, true);
-        } else {
-            // No esta permitido
-            callback(new Error("Error de Cors"));
+        try {
+            console.log('Origen:', origin); // Imprime el origen en la consola
+            if (whitelist.includes(origin)) {
+                // Puede consultar la API
+                callback(null, true);
+            } else {
+                // No esta permitido
+                throw new Error("Error de Cors");
+            }
+        } catch (error) {
+            console.error('Error:', error.message); // Imprime el error en la consola
+            callback(error);
         }
-    },
-    
+    }
 
     //origin:"*"
 };
