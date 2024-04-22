9eda9bff33ef90189268f239d1047e89ec4cdd11
CrisIntriago <caintria@espol.edu.ec>
2024-04-21 23:57:47 -0500

    tho

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ async function main() {
     try {
         //await sequelize.sync({ alter: true });
         console.log("La conexión a la base de datos se ha ejecutado correctamente");
-        app.listen(4000);
+        app.listen(port,"0.0.0.0");
         console.log("Server is listening on", 4000)
     } catch (error) {
         console.log("No se pudo conectar a la base de datos")
