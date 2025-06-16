70ec3b658e5f8709809b9e4fe720cee76787e9ed
Jair Ramírez <117552816+jairrami06@users.noreply.github.com>
2025-06-16 14:27:40 -0500

    Update index.js to remove code smells

--- src/index.js
+++ src/index.js
@@ -8,13 +8,10 @@ import bodegasRoutes from "../routes/bodegaRoutes.js";
 import modeloUnitarioRoutes from "../routes/modeloUnitarioRoutes.js"
 import movimientoUnitarioRoutes from "../routes/movimientoUnitarioRoutes.js";
 
-import { sequelize } from "../database/database.js";
-
 async function main() {
     const port = process.env.PORT || 3000;
 
     try {
-        //await sequelize.sync({ alter: true });
         console.log("La conexión a la base de datos se ha ejecutado correctamente");
         app.listen(port,"0.0.0.0");
         console.log("Server is listening on", 4000)
