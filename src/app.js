91cf78cadb3fb4e5b1686a5113424a9ce07c7afe
Jair Ramírez <117552816+jairrami06@users.noreply.github.com>
2025-06-16 14:36:04 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,45 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-
+import helmet from "helmet";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Middleware de seguridad: helmet para proteger encabezados HTTP
+app.use(helmet());
 
-const whitelist = [process.env.FRONTEND_URL , "190.154.6.111", "159.203.190.84"];
+// Lista blanca de orígenes permitidos
+const whitelist = [process.env.FRONTEND_URL, "190.154.6.111", "159.203.190.84"];
 
+// Configuración de CORS
 const corsOptions = {
-
-
-    /*
-    
     origin: function (origin, callback) {
-        try {
-            console.log('Origen:', origin); // Imprime el origen en la consola
-            if (whitelist.includes(origin)) {
-                // Puede consultar la API
-                callback(null, true);
-            } else {
-                // No esta permitido
-                throw new Error("Error de Cors");
-            }
-        } catch (error) {
-            console.error('Error:', error.message); // Imprime el error en la consola
-            callback(error);
+        if (whitelist.includes(origin) || !origin) {  // Permitir solicitudes sin origen (como Postman)
+            callback(null, true);
+        } else {
+            callback(new Error("No permitido por CORS"));
         }
     },
-    
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'], // Asegúrate de incluir 'PATCH' aquí
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    */
-    
-    origin:"*"
 };
 
+// Aplicar CORS a las solicitudes
 app.use(cors(corsOptions));
-
 
 export default app;
